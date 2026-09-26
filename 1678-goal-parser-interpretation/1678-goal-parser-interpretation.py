7c0dcd5a8450b79a607723425d43282b88d1b087
chirlasrisaicharantejareddy <25b11ai223@adityauniversity.in>
2026-09-26 09:39:20 +0530

    Time: 38 ms (89.01%), Space: 19.2 MB (46.58%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        s=""
+        for i in range(len(command)):
+            if command[i].isalpha():
+                s=s+command[i]
+            elif command[i]=='(' and command[i+1]==')':
+                s=s+'o'
+        return s
+        
